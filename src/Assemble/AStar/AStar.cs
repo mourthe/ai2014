042ff8fd8a8a26f756de18087d3a579427d09fca
Fabio Mourthé <mourthe@live.com>
2014-09-20 19:18:58 -0300

    commit para o arrais ser feliz

--- src/Assemble/AStar/AStar.cs
+++ src/Assemble/AStar/AStar.cs
@@ -8,5 +8,27 @@ namespace Assemble.AStar
 {
     public class AStar
     {
+        private Map _map;
+        private Point _startPoint;
+        private Point _endPoint;
+
+        private double _cost = 0;
+
+        public AStar(Map map, Point startPoint, Point endPoint)
+        {
+            _map = map;
+            _startPoint = startPoint;
+            _endPoint = endPoint;
+        }
+
+
+        public SearchResult Search()
+        {
+            var bestPath = new List<Point>();
+
+
+
+            return new SearchResult(_cost, bestPath);
+        }
     }
 }
---
+++ src/Assemble/AStar/AStarNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemble.AStar
+{
+    class AStarNode
+    {
+        public double Cost { get; private set; }
+
+        public AStarNode(double cost)
+        {
+            this.Cost = cost;
+        }
+    }
+}
