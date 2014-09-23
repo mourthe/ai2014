70cac8f4ab60a068d1576caba185b1a6ef15815e
Fabio Mourthé <mourthe@live.com>
2014-09-23 00:36:02 -0300

    v0.1 astar

--- src/Assemble/AStar/AStar.cs
+++ src/Assemble/AStar/AStar.cs
@@ -1,144 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Sorting;
-using Assemble;
 
-namespace Controller
+namespace Assemble.AStar
 {
     public class AStar
     {
-        private int[] dist;
-        private int[] path;
-      
-        private Map graph;
-        private int qtdNodes;
-        protected struct Elem
+        private readonly int[] _dist;
+        private readonly int[] _path;
+
+        private readonly Map _graph;
+        private readonly int _qtdNodes;
+
+        protected struct Element
         {
-            public Elem(int accCost, Point pos, Point? parent = null)
+            public Element(int accCost, Point pos, Point parent = null)
             {
-                this.accCost = accCost;
-                this.pos = pos;
-                this.parent = parent;
+                AccCost = accCost;
+                Pos = pos;
+                Parent = parent;
             }
 
-            public int accCost ;
-            public Point pos;
-            public Point? parent;
+            public int AccCost;
+            public Point Pos;
+            public Point Parent;
         }
 
         public AStar(Map graph)
         {
             /* initialize the array with infinity distance */
-            dist = Enumerable.Repeat(int.MaxValue, qtdNodes).ToArray() ;
-            path = Enumerable.Repeat(int.MinValue, qtdNodes).ToArray();
-            this.graph = graph;
-            this.qtdNodes = Map.Height * Map.Width;
+            _dist = Enumerable.Repeat(int.MaxValue, _qtdNodes).ToArray();
+            _path = Enumerable.Repeat(int.MinValue, _qtdNodes).ToArray();
+            _graph = graph;
+            _qtdNodes = Map.Height * Map.Width;
         }
 
-        private void SetNewDistance (int v, int u, int w)
+        private void SetNewDistance(int v, int u, int w)
         {
-            if (dist[u] > dist[v] + w)
-                    {
-                        /* we pass by v to get to u */
-                        dist[u] = dist[v] + w;
-                        path[u] = v;
-                    }
+            if (_dist[u] <= _dist[v] + w) return;
+            
+            /* we pass by v to get to u */
+            _dist[u] = _dist[v] + w;
+            _path[u] = v;
         }
 
-        public ICollection<Point> Star( Point posIni, Point posFinal, out int totalCost)
-        {            
-            var heapBorder = new Heap<Elem>();
+        public ICollection<Point> Star(Point posIni, Point posFinal, out int totalCost)
+        {
+            var heapBorder = new Heap<Element>();
+            var explored = new List<Element>();
 
-         //   Console.WriteLine("cheguei no astar");
-
-            List<Elem> explored = new List<Elem>();
             /* Array to verify if a position was explored */
-            var hasExpl = new bool[qtdNodes,qtdNodes];
-            var inBorder = new bool[qtdNodes,qtdNodes];
+            var hasExpl = new bool[_qtdNodes, _qtdNodes];
+            var inBorder = new bool[_qtdNodes, _qtdNodes];
             hasExpl.Initialize();
             inBorder.Initialize();
-            
-            
-            Elem father = new Elem(0, posIni);
-            heapBorder.HeapAdd( h(posIni,posFinal), father );
 
-
-            while (heapBorder.HeapSize() > 0 )
+            var father = new Element(0, posIni);
+            heapBorder.HeapAdd(H(posIni, posFinal), father);
+            
+            while (heapBorder.HeapSize() > 0)
             {
-                father = heapBorder.HeapExtractMin().Item3 ;
-                inBorder[father.pos.J, father.pos.I] = false;
-                if( father.pos.Equals(posFinal) )
+                father = heapBorder.HeapExtractMin().Item3;
+                inBorder[father.Pos.J, father.Pos.I] = false;
+                
+                if (father.Pos.Equals(posFinal))
                     break;
 
                 explored.Insert(0, father);
-                hasExpl[father.pos.J, father.pos.I] = true;
+                hasExpl[father.Pos.J, father.Pos.I] = true;
 
+                foreach (var child in _graph.GetNeighbors(posFinal))
+                {
+                    var accChild = 0;
+                    accChild = father.AccCost + 1;
 
-                foreach (var child in father.pos.Neighborhood( posFinal) )
-	            {
-                    int accChild = 0;
-                    accChild = father.accCost + 1;
-
-                    if (hasExpl[child.J, child.I] && accChild >= father.accCost)
+                    if (hasExpl[child.J, child.I] && accChild >= father.AccCost)
                         continue;
 
-                    if (inBorder[child.J, child.I] == false || accChild < father.accCost)
+                    if (inBorder[child.J, child.I] == false || accChild < father.AccCost)
                     {
-                        heapBorder.HeapAdd(h(child, posFinal) + accChild, new Elem(accChild, child, father.pos));
+                        heapBorder.HeapAdd(H(child, posFinal) + accChild, new Element(accChild, child, father.Pos));
                         inBorder[child.J, child.I] = true;
                     }
-	            }             
+                }
             }
 
-           
             var pathReturn = new List<Point>();
-            pathReturn.Insert(0, father.pos );
-            totalCost = father.accCost;
+            pathReturn.Insert(0, father.Pos);
+            totalCost = father.AccCost;
 
-            if (!father.parent.HasValue)
-               return pathReturn;
+            if (!father.Parent.HasValue)
+                return pathReturn;
 
-            var currParent = father.parent.Value ;
-           
-
-           
-           
+            var currParent = father.Parent.Value;
             
-            for (int i = 0 , j = 1; i < explored.Count; i++)
-			{
-                if (explored[i].pos.Equals(currParent) )
+            for (int i = 0, j = 1; i < explored.Count; i++)
+            {
+                if (explored[i].Pos.Equals(currParent))
                 {
-                    pathReturn.Insert(j,explored[i].pos);
+                    pathReturn.Insert(j, explored[i].Pos);
                     j++;
-                    currParent = explored[i].parent.HasValue ? explored[i].parent.Value : posIni  ;
-                    //Debug.WriteLine("custo "+explored[i].accCost);
+                    currParent = explored[i].Parent.HasValue ? explored[i].Parent.Value : posIni;
                 }
-			}
+            }
             pathReturn.Reverse();
             return pathReturn.Skip(1).ToList();
 
         }
 
-
-        public int h(Point posIni, Point posFin)
+        public int H(Point posIni, Point posFin)
         {
-            int jIni = posIni.J ;
-            int iIni = posIni.I ;
-            int jFin = posFin.J ;
-            int iFin = posFin.I ;
+            var jIni = posIni.J;
+            var iIni = posIni.I;
+            var jFin = posFin.J;
+            var iFin = posFin.I;
 
-            return (int)Math.Sqrt( Math.Pow( (jIni - jFin), 2) + Math.Pow( (iIni - iFin), 2))  ;
+            return (int)Math.Sqrt(Math.Pow((jIni - jFin), 2) + Math.Pow((iIni - iFin), 2));
         }
 
-        
         private Terrain GetTerrainFromPos(Point pos)
         {
-          return graph.Points[pos.I, pos.J].Terrain;
+            return _graph.Points[pos.I, pos.J].Terrain;
         }
-    
     }
 }
