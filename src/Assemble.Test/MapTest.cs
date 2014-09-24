4906977967810e78a804f2e39ecb031ceff24b9f
Gabriel Arrais <gabriel.arrais@vtex.com.br>
2014-09-24 14:34:27 -0300

    add classe pra teste de Map

---
+++ src/Assemble.Test/MapTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemble.Test
+{
+    [TestFixture]
+    public class MapTest
+    {
+        [Test]
+        public 
+    }
+}
