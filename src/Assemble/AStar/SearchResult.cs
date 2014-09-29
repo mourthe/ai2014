39524cfc75c107b7cef09a3a5955f20a9c138c97
Fabio Mourthé <mourthe@live.com>
2014-09-29 13:14:41 -0300

    cria search result

---
+++ src/Assemble/AStar/SearchResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemble.AStar
+{
+    public class SearchResult
+    {
+        public SearchResult(int cost, List<Point> bestPath)
+        {
+            BestPath = bestPath;
+            Cost = cost;
+        }
+
+        public int Cost { get; private set; }
+
+        public List<Point> BestPath { get; private set; }
+    }
+}
