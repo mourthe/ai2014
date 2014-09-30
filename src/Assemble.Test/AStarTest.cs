16dec4dbf49a54ef1ffb1dfaece79f26a8d18168
Fabio Mourthé <mourthe@live.com>
2014-09-30 01:03:14 -0300

    Adiciona teste de matriz 10 x 10

--- src/Assemble.Test/AStarTest.cs
+++ src/Assemble.Test/AStarTest.cs
@@ -99,6 +99,62 @@ namespace Assemble.Test
             Assert.That(result.Cost, Is.EqualTo(33));
         }
 
+        [Test]
+        public void AStar_should_return_the_best_path_ten_by_ten()
+        {
+            // arrange
+            var terrain = new List<int>
+            {
+                101, 101, 495, 171, 171, 101, 101, 495, 171, 171,
+                101, 495, 495, 171, 101, 101, 495, 495, 171, 101,
+                354, 171, 495, 491, 101, 354, 171, 495, 491, 101,
+                354, 171, 171, 491, 171, 495, 171, 171, 491, 171,
+                495, 495, 354, 171, 171, 495, 354, 354, 171, 171,
+                101, 101, 495, 171, 171, 101, 101, 495, 171, 171,
+                101, 495, 495, 171, 101, 101, 495, 495, 171, 101,
+                354, 171, 495, 491, 101, 354, 171, 495, 491, 101,
+                354, 171, 171, 491, 171, 354, 171, 171, 491, 171,
+                495, 495, 354, 171, 171, 495, 495, 354, 171, 171
+            };
+
+            var map = new Map(terrain, CreateCharacters(), 10);
+            var aStar = new AStar.AStar(map);
+
+            // act
+            var result = aStar.Star(map.Points[6, 4], map.Points[0, 1]);
+
+            // assert
+            Assert.That(result.Cost, Is.EqualTo(28));
+        }
+
+                [Test]
+        public void AStar_should_return_the_best_path_ten_by_ten_just_to_be_sure()
+        {
+            // arrange
+            var terrain = new List<int>
+            {
+                101, 101, 495, 171, 171, 101, 101, 495, 171, 171,
+                101, 495, 495, 171, 101, 101, 495, 495, 171, 101,
+                354, 171, 495, 491, 101, 354, 171, 495, 491, 101,
+                354, 171, 171, 491, 171, 495, 171, 171, 491, 171,
+                495, 495, 354, 171, 171, 495, 354, 354, 171, 171,
+                101, 101, 495, 171, 171, 101, 101, 495, 171, 171,
+                101, 495, 495, 171, 101, 101, 495, 495, 171, 101,
+                354, 171, 495, 491, 101, 354, 171, 495, 491, 101,
+                354, 171, 171, 491, 171, 354, 171, 171, 491, 171,
+                495, 495, 354, 171, 171, 495, 495, 354, 171, 171
+            };
+
+            var map = new Map(terrain, CreateCharacters(), 10);
+            var aStar = new AStar.AStar(map);
+
+            // act
+            var result = aStar.Star(map.Points[8, 1], map.Points[3, 9]);
+
+            // assert
+            Assert.That(result.Cost, Is.EqualTo(34));
+        }
+
         private static List<Character> CreateCharacters()
         {
             var characters = new List<Character>();
